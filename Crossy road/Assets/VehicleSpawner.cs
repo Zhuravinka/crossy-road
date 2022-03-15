9064138ccf1ddb3db592650af8fb8bfd4c0e6564
Ellen <lemifi@mail.ru>
2022-03-15 13:06:07 +0300

    reworked the prefabs

--- Crossy road/Assets/VehicleSpawner.cs
+++ Crossy road/Assets/VehicleSpawner.cs
@@ -10,13 +10,17 @@ public class VehicleSpawner : MonoBehaviour
     [SerializeField] float _minSpawnTime = 3f;
     [SerializeField] float _maxSpawnTime = 5f;
 
-    void Update()
+    void Start()
     {
         StartCoroutine(SpawnCar(Random.Range(_minSpawnTime, _maxSpawnTime)));
     }
     IEnumerator SpawnCar (float spawnTime)
     {
-        var newVehicle = Instantiate(vehiclePrefab, vehiclePrefab.transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(spawnTime);
+        for (int i = 0; i < 10; i++)
+        {
+            var newVehicle = Instantiate(vehiclePrefab, vehiclePrefab.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnTime);
+        }
+       
     }
 }
